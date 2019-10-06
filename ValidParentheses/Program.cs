362bcebde007c621e1574717be4562d8641483d0
Matthew Sawrey <matski53@gmail.com>
2019-10-06 15:04:47 +0100

    Added ValidParenthese problem solution

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tests = new[]
+            {
+                new { TestString = "()", ExpectedValue = true}
+                , new { TestString = "()[]{}", ExpectedValue = true}
+                , new { TestString = "(]", ExpectedValue = false}
+                , new { TestString = "([)]", ExpectedValue = false}
+                , new { TestString = "{[]}", ExpectedValue = true}
+                , new { TestString = "", ExpectedValue = true}
+                , new { TestString = "]", ExpectedValue = false}
+            };
+
+            Solution solution = new Solution();
+
+            for (int i = 0; i < tests.Length; i++)
+            {
+                var result = solution.IsValid(tests[i].TestString);
+                var wasTestSuccessfull = result == tests[i].ExpectedValue;
+                
+                Console.WriteLine($"Test {i} has {(wasTestSuccessfull ? "succeeded" : "failed.")}");
+            }
+        }
+    }
+
+    public class Solution
+    {
+        static char[] OpeningParentheses = new char [] { '(', '[', '{' };
+
+        public bool IsValid(string s)
+        {
+            var chars = s.ToCharArray();
+            if(chars.Length == 0)
+                return true;
+
+            Stack<char> expectedClosingParentheses = new Stack<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                // Add opening parenthesis to list
+                if(isOpeningParentheses(chars[i]))
+                {
+                    expectedClosingParentheses.Push(GetClosingParenthesesFromOpening(chars[i]));
+                    continue;
+                }
+
+                // If not opening parentheses, must be closing. Therefore check it correctly closes the latest opening
+                if(expectedClosingParentheses.Count == 0)
+                {
+                    // found the incorrect closing parentheses
+                    return false;                    
+                }
+                else if(chars[i] == expectedClosingParentheses.Peek())
+                {
+                    expectedClosingParentheses.Pop();
+                    continue;
+                }
+                else
+                {
+                    // found the incorrect closing parentheses
+                    return false;
+                }
+            }
+            // Reached the end of the char array and there are still unclosed pairs
+            return expectedClosingParentheses.Count == 0;
+        }
+
+        private bool isOpeningParentheses(char value)
+        {
+            for (int i = 0; i < OpeningParentheses.Length; i++)
+            {
+                if(OpeningParentheses[i] == value)
+                    return true;
+            }
+            return false;
+        }
+
+        private char GetClosingParenthesesFromOpening(char openingParentheses)
+        {
+            switch(openingParentheses)
+            {
+                case '(':
+                    return ')';
+                case '{':
+                    return '}';
+                case '[':
+                    return ']';
+                default:
+                    throw new Exception("Not a valid char supplied!");
+            }
+        }
+    }
+}
