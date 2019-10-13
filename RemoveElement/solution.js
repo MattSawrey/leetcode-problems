dca9d040fc8009f0a07fef22c295e200d7ea0c6a
Matthew Sawrey <matski53@gmail.com>
2019-10-13 14:05:52 +0100

    Added RemoveElement problem solution (JS).

---
+++ RemoveElement/solution.js
@@ -0,0 +1,28 @@
+
+/*
+https://leetcode.com/problems/remove-element/
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+// var nums = [2,3,3,2];
+// var val = 3;
+
+var nums = [0,1,2,2,3,0,4,2];
+var val = 2;
+
+console.log(nums.length);
+
+var removeElement = function(nums, val) {
+	for(var i = nums.length - 1; i >= 0; i--){
+		if(nums[i] === val) {
+			nums.splice(i, 1);
+		}
+	}
+	return nums.length;
+};
+
+console.log(removeElement(nums, val));
+console.log(nums);
+
