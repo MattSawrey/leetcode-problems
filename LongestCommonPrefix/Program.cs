fcc539562f3e302d2b50e5204a33acc672215f06
Matthew Sawrey <matski53@gmail.com>
2020-05-08 10:29:04 +0100

    Added LongestCommonPrefix solution.

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LongestCommonPrefix
+{
+    //Write a function to find the longest common prefix string amongst an array of strings.
+
+    //If there is no common prefix, return an empty string "".
+
+    //Example 1:
+
+    //Input: ["flower","flow","flight"]
+    //Output: "fl"
+
+    //Example 2:
+
+    //Input: ["dog","racecar","car"]
+    //Output: ""
+    //Explanation: There is no common prefix among the input strings.
+
+    //Note:
+
+    //All given inputs are in lowercase letters a-z.
+
+    class Program
+    {
+        static string[] test1 = new string[] { "flower", "flow", "flight" };
+        static string[] test2 = new string[] { "dog", "racecar", "car" };
+        static string[] test3 = new string[] { };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestCommonPrefix(test1) + ". Should = 'fl'");
+            Console.WriteLine(LongestCommonPrefix(test2) + ". Should = ''");
+            Console.WriteLine(LongestCommonPrefix(test3) + ". Should = ''");
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            char[] commonPrefixCharArray = null;
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (i == 0)
+                {
+                    commonPrefixCharArray = strs[i].ToCharArray();
+                    continue;
+                }
+                char[] charArray = strs[i].ToCharArray();
+                for (int x = 0; x < commonPrefixCharArray.Length; x++)
+                {
+                    //if the compare to array 
+                    if (x > charArray.Length - 1 || commonPrefixCharArray[x] != charArray[x])
+                    {
+                        Array.Resize(ref commonPrefixCharArray, x);
+                        continue;
+                    }
+                }
+            }
+            return new string(commonPrefixCharArray);
+        }
+    }
+}
---
+++ Permutations/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    class Program
+    {
+        // Given a collection of distinct integers, return all possible permutations
+        // Input: [1,2,3]
+        // Output:
+        // [
+        //   [1,2,3],
+        //   [1,3,2],
+        //   [2,1,3],
+        //   [2,3,1], 
+        //   [3,1,2],
+        //   [3,2,1]
+        // ]
+
+        static int[] nums = { 1, 2, 3 };
+
+        static void Main(string[] args)
+        {
+            var permutations = Permute(nums).ToArray();
+
+            for(int i = 0; i < permutations.Length; i++)
+            {
+                for(int y = 0; y < permutations[i].Count; y++)
+                {
+                    Console.WriteLine(permutations[i].ToArray().ArrayToString());
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        public static IList<IList<int>> Permute(int[] nums) 
+        {
+            // Formula for total number of possible permutations.
+            // Loop over every element in an array, placing that element at the start of the returned permutation.
+            // Then the number of possible permuations from that elements is
+
+
+            IList<IList<int>> permutations = new List<IList<int>>();
+
+            // For each element in the array
+            for(int i = 0; i < nums.Length; i++)
+            {
+                List<int> currentPermutation = new List<int>();
+                currentPermutation.AddRange(nums.ToList());
+
+                int currentElement = currentPermutation[i];
+                currentPermutation.RemoveAt(i);
+
+                // Shift each value iteratively back
+                for(int q = 0; q < nums.Length; q++)
+                {
+                    int indexToInsert = q;
+                    if(q > i)
+                        indexToInsert--;
+
+                    currentPermutation.Insert(indexToInsert, currentElement);
+                    permutations.Add(currentPermutation);
+                }
+            }
+
+            return permutations;
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static string ArrayToString (this int[] array)
+        {
+            string text = "[";
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(i != 0)
+                    text += ", ";
+                text += array[i];
+            }
+            text += "]";
+            return text;
+        }
+    }
+}
